fe481264030cb326a4b901e1d389ee3ebb01d923
donpachi <isikodon@googlemail.com>
2022-01-21 15:49:17 +0000

    update to UML and comments

--- src/main/java/OrderStorage.java
+++ src/main/java/OrderStorage.java
@@ -1,6 +1,7 @@
 public class OrderStorage {
 
-    // to store objects from Manager
-    // transfer to inputImplementer
+    // to store drink objects from Manager
+    // to store stock information from Manager
+    // transfer information to inputImplementer via get methods
 
 }
