dd699b260fa5bc84f2d2eeb27d30be208f2b4663
donpachi <isikodon@googlemail.com>
2022-01-21 11:54:59 +0000

    draw io file

--- src/main/java/InputImplementer.java
+++ src/main/java/InputImplementer.java
@@ -1,4 +1,15 @@
 public class InputImplementer {
 
+    /*
+     * what does this class need?
+     * what are the variables
+     * what are the dependencies
+     * write these down for each methods
+     *
+     *
+     *
+     *
+     * */
+
     //
 }
