f7184a8d905379b34ab14a1679bd6adb9483615a
donpachi <isikodon@googlemail.com>
2022-02-06 21:21:03 +0000

    project is being refactored to a Spring Boot application, initial commit of refactoring process

---
+++ documents/user setup.sql
@@ -0,0 +1,19 @@
+use dimmbar;
+
+-- for testing purposes only
+ CREATE USER 'dev'@'localhost';
+GRANT ALL ON dimmbar.* TO 'dev'@'localhost';
+
+CREATE USER 'dimmbaradmin'@'localhost' IDENTIFIED BY 'D1mb@rr@dm1n1342';
+GRANT ALL ON dimmbar.* TO 'dimmbaradmin'@'localhost';
+
+CREATE USER 'dimmbarmanager'@'localhost' IDENTIFIED BY 'D1mb@rrman@g3r9468';
+GRANT INSERT ON dimmbar.* TO 'dimmbarmanager'@'localhost';
+GRANT SELECT ON dimmbar.* TO 'dimmbarmanager'@'localhost';
+GRANT UPDATE ON dimmbar.* TO 'dimmbarmanager'@'localhost';
+GRANT DELETE ON dimmbar.* TO 'dimmbarmanager'@'localhost';
+
+CREATE USER 'dimmbarstaff'@'localhost' IDENTIFIED BY 'D1mb@rrst@ff';
+GRANT SELECT ON dimmbar.* TO 'dimmbarstaff'@'localhost';
+
+git commit -m "QDFP-6 #in-progress #time 2h 0m #comment completed integration testing for Job CRUD"
